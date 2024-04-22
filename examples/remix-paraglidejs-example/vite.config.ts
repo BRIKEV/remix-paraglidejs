1d1308ab975ff2ae0dd772ac1ef245ecfcc499af
LorisSigrist <loris@sigrist.dev>
2024-04-22 09:21:23 +0200

    Use `@inlang/paraglide-vite`

--- examples/remix-paraglidejs-example/vite.config.ts
+++ examples/remix-paraglidejs-example/vite.config.ts
@@ -2,7 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { paraglide } from "@inlang/paraglide-js-adapter-vite";
+import { paraglide } from "@inlang/paraglide-vite";
 
 installGlobals();
 
